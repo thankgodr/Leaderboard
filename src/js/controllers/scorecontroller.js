efb01fcdd2bb503dad4888c2a8192de7c76335e9
ThankGod Richard <madueketf@gmail.com>
2021-10-06 04:31:40 +0100

    Demo data dynamically generated

--- src/js/controllers/scorecontroller.js
+++ src/js/controllers/scorecontroller.js
@@ -2,14 +2,24 @@ import Score from '../model/score';
 export default class ScoreController {
   constructor(scoresArray) {
     this.scoresArray = scoresArray;
+    //Update index
+    this.scoresArray.forEach((element, index) => {
+      element.updateIndex(index);
+    });
   }
 
   populateDemo(amount) {
     let num = 0;
     while (num < amount) {
-      let tempScore = new Score(`Demo ${num + 1}`);
+      let ranNum = Math.round(Math.random() * (100 - 1) + 1);
+      let tempScore = new Score(`Demo ${num + 1}`, ranNum);
       this.scoresArray.push(tempScore);
       num++;
     }
+  }
+
+  addScore(task) {
+    task.updateIndex(this.scoresArray.length);
+    this.scoresArray.push(task);
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,19 @@
+import ScoreController from './controllers/scorecontroller';
 import '../css/style.css';
-console.log('yes');
+
+let scoreController;
+
+function buildDemoView() {
+  scoreController = new ScoreController([]);
+  scoreController.populateDemo(5);
+  let domScoresHolders = document.getElementById('scoreHolder');
+  scoreController.scoresArray.forEach((element, loopIndex) => {
+    let singleScore = document.createElement('li');
+    singleScore.className = loopIndex % 2 == 0 ? 'list-group-item' : 'list-group-item disabled';
+    let textNode = document.createTextNode(`Name: ${element.name} Score: ${element.score}`);
+    singleScore.appendChild(textNode);
+    domScoresHolders.appendChild(singleScore);
+  });
+}
+
+buildDemoView();
